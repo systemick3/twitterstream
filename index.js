9556385e32d54c564c476bce1a1d7f8f83254ab6
Mike Garthwaite <michaelgarthwaite@gmail.com>
2015-04-12 09:04:42 +0100

    Removed headers that didn't work

--- index.js
+++ index.js
@@ -20,14 +20,6 @@ app.set('env', process.env.NODE_ENV || config.env || 'development');
 app.use(morgan("combined", {stream: logfile}));
 app.use(morgan("dev"));
 
-//app.use(function (req, res, next) {
-  //res.header("Access-Control-Allow-Origin", req.headers.origin);
-  //res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-  //res.header("Access-Control-Allow-Credentials", "true");
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  //next();
-//});
-
 // Create server
 server = http.createServer(app);
 
